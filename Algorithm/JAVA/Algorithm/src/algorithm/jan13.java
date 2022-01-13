7f20456a6252afb154e3f18bd82b991eab3270a8
miju0515 <76895039+miju0515@users.noreply.github.com>
2022-01-13 22:25:27 +0900

    jan 13
    #BOJ 10871
    #BOJ 2439

---
+++ Algorithm/JAVA/Algorithm/src/algorithm/jan13.java
@@ -0,0 +1,34 @@
+package algorithm;
+import java.util.Scanner;
+public class jan13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//10871
+		/*Scanner s = new Scanner(System.in);
+		int test=s.nextInt();
+		int boundary=s.nextInt();
+		for(int i=0;i<test;i++) {
+			int number=s.nextInt();
+			if(number<boundary) {
+				System.out.print(number+" ");
+			}
+		}
+		s.close();
+		*/
+		//2439
+		/*Scanner s=new Scanner(System.in);
+		int number=s.nextInt();
+		for(int i=1;i<=number;i++) {
+			for(int j=0;j<number-i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=0;k<i;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		s.close();*/
+	}
+
+}
