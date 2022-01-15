77a91ed26d77ceed10bececafa84535cdb227bf2
miju0515 <76895039+miju0515@users.noreply.github.com>
2022-01-15 23:57:51 +0900

    jan 15
    #BOJ 2743

---
+++ Algorithm/JAVA/Algorithm/src/algorithm/jan15.java
@@ -0,0 +1,13 @@
+package algorithm;
+import java.util.Scanner;
+public class jan15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		String word=s.next();
+		System.out.print(word.length());
+		s.close();
+	}
+
+}
