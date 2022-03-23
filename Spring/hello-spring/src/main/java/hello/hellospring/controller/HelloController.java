432220c3111b22f788bcf715fb278734752f3b2b
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2022-03-24 02:00:32 +0900

    March 23
    learn about Spring

--- Spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ Spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,17 +3,21 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-	
-	@GetMapping("hello")
-	public String hello(Model model) {
-		model.addAttribute("data", "hello!");
-		//model.addAttribute(attributeName:"data", attributeValue:"hello!!");
-		return "hello";
-		//컨트롤러에서 리턴 값으로 문자를 반환하면 뷰 리졸버('viewResolver')가 화면을 찾아서 처리한다.
-		//스프링부트 템플릿엔진 기본 viewName 매핑
-		//'resource:templates/'+(ViewName)+'.html'
-	}
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","hello!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name,Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
 }
