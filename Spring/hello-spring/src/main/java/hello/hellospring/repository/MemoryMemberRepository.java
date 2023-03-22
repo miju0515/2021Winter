09332204855c83adfd2dd736cd0fa38e461de1c5
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-03-22 13:41:41 +0900

    230315
    스프링공부

--- Spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ Spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -19,16 +19,24 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public Optional<Member> findById(Long id) {
+
         return Optional.ofNullable(store.get(id));
     }
 
     @Override
     public Optional<Member> findByName(String name) {
-        return store.values().stream().filter(member ->member.getName().equals(name)).findAny();
+        return store.values().stream()
+                .filter(member ->member.getName().equals(name))
+                .findAny();
     }
 
     @Override
     public List<Member> findAll() {
+
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
     }
 }
