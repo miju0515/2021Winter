39e448d58b85f13319339fe21a901685781ce182
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-03-23 17:10:24 +0900

    230323
    스프링 입문
    -JPA

--- Spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,24 +1,27 @@
 package hello.hellospring;
 
 
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManager;
 
 //직접 스프링 빈 등록하기
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
+
+
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource){
-        this.dataSource=dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -28,6 +31,7 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
 
         //return new MemoryMemberRepository();
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
